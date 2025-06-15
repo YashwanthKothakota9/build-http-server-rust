807ec347051a489b3273b9d957a450dc748ddc27
YashwanthK <yashwanthkothakota@gmail.com>
2025-06-15 19:28:07 +0530

    add: support for concurrent connections using threads

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::net::TcpListener;
 use std::{
     collections::HashMap,
     io::{Read, Write},
+    net::TcpStream,
+    thread,
 };
 
 #[derive(Debug)]
@@ -53,30 +55,35 @@ fn response_with_body(body: &str) -> String {
     )
 }
 
+fn handle_connection(stream: &mut TcpStream) {
+    println!("accepted new connection");
+    let mut buffer = [0; 1024];
+    let bytes_read = stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8_lossy(&buffer[..bytes_read]);
+    let request_parsed = request_parser(&request);
+    let path = &request_parsed.path;
+    let response = if path == "/" {
+        "HTTP/1.1 200 OK\r\n\r\n".to_string()
+    } else if path == "/user-agent" {
+        response_with_body(request_parsed.headers.get("User-Agent").unwrap())
+    } else if path.starts_with("/echo") {
+        response_with_body(&path[6..])
+    } else {
+        "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+    };
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
-                let mut buffer = [0; 1024];
-                let bytes_read = _stream.read(&mut buffer).unwrap();
-                let request = String::from_utf8_lossy(&buffer[..bytes_read]);
-                let request_parsed = request_parser(&request);
-                println!("Request: {:?}", request_parsed);
-                let path = &request_parsed.path;
-                let response = if path == "/" {
-                    "HTTP/1.1 200 OK\r\n\r\n".to_string()
-                } else if path == "/user-agent" {
-                    response_with_body(request_parsed.headers.get("User-Agent").unwrap())
-                } else if path.starts_with("/echo") {
-                    response_with_body(&path[6..])
-                } else {
-                    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
-                };
-                _stream.write_all(response.as_bytes()).unwrap();
-                _stream.flush().unwrap();
+                thread::spawn(move || {
+                    handle_connection(&mut _stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
